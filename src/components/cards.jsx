26fbbaad2c6138c6a3bd39d5c6e431c82c726e45
caetanaldo <gustavocaetanodalponte@gmail.com>
2025-11-25 09:19:12 -0300

    Update cards.jsx

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -2,7 +2,6 @@ export default function Card({ titulo, imagem, preco }){
 
     return(
         <div>
-
             <img src={imagem} alt={titulo} />
             <h3> {titulo} </h3>
             <strong>RS ${preco}</strong>
